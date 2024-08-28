c21222016f3ad59cfea162702f831b88fd4540b0
Hope <hopemwaura39@gmail.com>
2024-08-28 08:00:16 +0300

    completed lab

--- lib/functions.py
+++ lib/functions.py
@@ -14,3 +14,17 @@ def add(num1, num2):
 
 def halve(number):
     pass
+def greet_programmer():
+    print("Hello, programmer!")
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+
+def add(num1, num2):
+    return 45+55
+
+def halve(int):
+    return int/2
